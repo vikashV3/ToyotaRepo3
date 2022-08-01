e98f4d856c68fd5052c8ede3f405d6830cedc949
vikash.mishra <vikash39873@gmail.com>
2022-08-01 14:29:38 +0530

    Firt Method Added

--- src/main/java/com/example/controller/DemoController.java
+++ src/main/java/com/example/controller/DemoController.java
@@ -1,5 +1,17 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestController
 public class DemoController {
 
-}
+	
+	
+		@RequestMapping(value = "/")
+			public String method1() {
+				return("Hi");
+			}
+		}
+
